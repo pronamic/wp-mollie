b3d21bd64e5c601bea4e5cdc95d66652f52f849e
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-09-06 10:15:47 +0200

    Add refund class test, to test JSON validation.
    https://github.com/pronamic/pronamic-pay-with-mollie-for-woocommerce/issues/6

---
+++ tests/src/RefundTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Mollie refund test
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
+
+namespace Pronamic\WordPress\Mollie;
+
+use Yoast\PHPUnitPolyfills\TestCases\TestCase;
+
+/**
+ * Mollie refund test class
+ */
+class RefundTest extends TestCase {
+	/**
+	 * Test JSON.
+	 */
+	public function test_json() {
+		$json_file = __DIR__ . '/../json/refund.json';
+
+		$json_data = json_decode( file_get_contents( $json_file, true ) );
+
+		$refund = Refund::from_json( $json_data );
+
+		$this->assertInstanceOf( Refund::class, $refund );
+	}
+}
